3aa6eb4b80ac23cbde527d080e5d4efd0e6fbf93
Vatsal Gupta <Vasu@Gunjans-MacBook-Air.local>
2023-06-04 16:10:35 +0530

    adding files

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -1,46 +1,79 @@
-import React, { useEffect} from 'react'
-import { useState } from 'react'
-// import io from 'socket.io-client'
-import './Chat.css'
+import React, { useState } from 'react';
+import './Chat.css';
+import { MdClose, MdChat } from 'react-icons/md';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+const Chat = () => {
+  const [showModel, setShowModel] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
-const Chat =() => {
-    var [showModel, setShowModel] = React.useState(false)
+  const closePopup = () => {
+    setShowModel(false);
+  };
 
-    return (
-        <div>
-            <div className="bubble"  >
-                <button class="open-button" onClick={() => {setShowModel(true) }}>Chat</button>
+  const openPopup = () => {
+    setShowModel(true);
+    setSubmitted(false);
+  };
 
-            </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+    setShowModel(false);
+    toast.success('Thank you! We will contact you shortly.', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+  };
 
-
-            
-                <div class="chat-popup" id="myForm" style={{display: showModel ? 'block' : 'none'}}>
-                    <form class="form-container">
-                        <h1>Chat</h1>
-
-                        <label for="msg"><b>Message</b></label>
-                        <textarea placeholder="Type message.." name="msg" required></textarea>
-
-                        <button type="submit" class="btn">Send</button>
-                        <button type="button" class="btn cancel" onClick={()=>{setShowModel(false)}}>Close</button>
-                    </form>
-                </div>
-            
-            
-
-            <div onClick={() => setShowModel(false)}/>
-            {/* <div onClick={() => setShowModel(false)} >
-                <div className="cross">
-                    /*<img className='close_button' src="" alt="close_button" />
-
-                </div>
-
-            </div> */}
+  return (
+    <div className="bubble">
+      {!showModel && (
+        <div className="chat-icon" onClick={openPopup}>
+          <MdChat />
         </div>
-    )
-}
-export default Chat
+      )}
 
+      <div className="chat-popup" id="myForm" style={{ display: showModel ? 'block' : 'none' }}>
+        <div className="close-icon" onClick={closePopup}>
+          <MdClose />
+        </div>
+        <form className="form-container" onSubmit={handleSubmit}>
+          <h1>Hi, please enter your details and we will contact you soon</h1>
 
+          <label htmlFor="name">
+            <b>Name</b>
+          </label>
+          <textarea type="text" placeholder="Enter your name" name="name" required />
+
+          <label htmlFor="email">
+            <b>Email</b>
+          </label>
+          <textarea type="email" placeholder="Enter your email" name="email" required />
+
+          <label htmlFor="mobile">
+            <b>Mobile Number</b>
+          </label>
+          <textarea type="text" placeholder="Enter your mobile number" name="mobile" required />
+
+          <button type="submit" className="btn">
+            Send
+          </button>
+
+          
+        </form>
+      </div>
+
+      <ToastContainer autoClose={3000} />
+    </div>
+    
+  );
+};
+
+export default Chat;
